f1230fd69a36a933653445f4f097ca03bd4d11ab
Scarjit <scarjit@aol.com>
2015-01-14 14:36:19 +0100

    0.0.2
    -Added .doc# to gitignore

---
+++ src/progpro/Globals.java
@@ -0,0 +1,8 @@
+package progpro;
+/*
+ * Diese Datei ist für alle Globale Variabeln.
+ * Bitte verwendet privates oder protected mit getter/setter struktur.
+ */
+public class Globals {
+
+}
---
+++ src/progpro/Main.java
@@ -0,0 +1,6 @@
+package progpro;
+public class Main {
+   public static void main(String[] a) {
+	System.out.println("Hello World");   
+   }  
+}

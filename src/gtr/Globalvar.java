4f5e1fdbe6baff15c7572d0c78be7aa0b7fa535b
Scarjit <scarjit@aol.com>
2015-02-16 16:56:37 +0100

    1.0.2
    QWQ

---
+++ src/gtr/Globalvar.java
@@ -0,0 +1,13 @@
+package gtr;
+
+public class Globalvar {
+	String[] graphen;
+
+	public String[] getGraphen() {
+		return graphen;
+	}
+
+	public void addGraphen(String[] graphen) {
+		this.graphen = graphen;
+	}
+}
